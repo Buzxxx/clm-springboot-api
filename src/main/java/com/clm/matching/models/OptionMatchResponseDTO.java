e2c0fa2a77987ffba24d7cb54039e4cbaf2cbc44
Vishal sharma <bishalfirstahuman@gmail.com>
2025-01-31 23:15:28 +0530

    Match Overview API For the First Page display

--- src/main/java/com/clm/matching/models/OptionMatchResponseDTO.java
+++ src/main/java/com/clm/matching/models/OptionMatchResponseDTO.java
@@ -1,11 +1,15 @@
 package com.clm.matching.models;
 
 import com.clm.category.models.OptionDTO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OptionMatchResponseDTO {
 
     private Long id;
