426a5e58d4ad30526e3677b06af9ee0165f5a126
Vishal sharma <bishalfirstahuman@gmail.com>
2025-03-25 09:22:54 +0530

    Changing the name to plural

--- src/main/java/com/clm/category/api/AppTypeController.java
+++ src/main/java/com/clm/category/api/AppTypeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/appType")
+@RequestMapping("/api/v1/appTypes")
 public class AppTypeController {
 
     private final AppTypeService appTypeService;
